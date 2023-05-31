16195b4bfd9881a69d5518c76fd6bbc65088abd7
Aiden Luce <luceaiden4124@gmail.com>
2023-05-31 16:11:13 -0500

    Tours component

--- src/data.js
+++ src/data.js
@@ -1,3 +1,9 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
+
 export const pageLinks = [
     {id: 1, href: '#home', text: 'home'},
     {id: 2, href: '#about', text: 'about'},
@@ -27,5 +33,51 @@ export const services =[
         icon: 'fas fa-socks fa-fw',
         title: 'amazing comfort',
         text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.'
+    },
+]
+export const tours = [
+    {
+        id:1,
+        img: tour1,
+        date: 'august 26th, 2020',
+        title: 'Tibet Adventure',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'china',
+        icon: 'fas fa-map',
+        duration: '6 days',
+        price: 'from $2100',
+    },
+    {
+        id:2,
+        img: tour2,
+        date: 'october 1st, 2020',
+        title: 'best of java',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'indonesia',
+        icon: 'fas fa-map',
+        duration: '11 days',
+        price: 'from $1400',
+    },
+    {
+        id:3,
+        img: tour3,
+        date: 'september 15th, 2020',
+        title: 'explore hong kong',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'hong kong',
+        icon: 'fas fa-map',
+        duration: '8 days',
+        price: 'from $5000',
+    },
+    {
+        id:4,
+        img: tour4,
+        date: 'december 5th, 2019',
+        title: 'kenya highlights',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+        location: 'kenya',
+        icon: 'fas fa-map',
+        duration: '20 days',
+        price: 'from $3300',
     },
 ]
